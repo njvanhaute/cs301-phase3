40df1fecacd946f6281ddd0f4c715eb561250054
swmaryland <swmaryland@crimson.ua.edu>
2018-11-29 15:00:18 -0600

    manager password table questions

--- login.php
+++ login.php
@@ -7,22 +7,29 @@
       
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT Username FROM CUSTOMER WHERE Username = '$myusername' and Password_Customer = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
+
+      $sql_customer = "SELECT Username FROM CUSTOMER WHERE Username = '$myusername' AND Password_Customer = '$mypassword'";
+      $result_customer = mysqli_query($db, $sql_customer);
+      $row_customer = mysqli_fetch_array($result_customer, MYSQLI_ASSOC);
+      $active_customer = $row_customer['active'];
+
+      $sql_manager = "SELECT Username FROM MANAGER WHERE Username = '$myusername' AND Password_Manager = '$mypassword'";
+      $result_manager = mysqli_query($db, $sql_manager);
+      $row_manager = mysqli_fetch_array($result_manager, MYSQLI_ASSOC);
+      $active_manager = $row_manager['active'];
+
+      $count_customer = mysqli_num_rows($result_customer);
+      $count_manager = mysqli_num_rows($result_manager);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
-      if($count == 1) {
-         $_SESSION['login_user'] = $myusername;
-         
-         header("Location: welcome.php");
-      }else {
-        $error = "Your Login Name or Password is invalid";
+      if ($count_customer == 1 || $count_manager == 1) {
+        $_SESSION['login_user'] = $myusername;
+        header("location: welcome.php");
+      }
+      else {
+        $message = "Incorrect Username/Password";
+        echo "<script type='text/javascript'>alert('$message');</script>";
       }
    }
 ?>
@@ -31,8 +38,8 @@
   <body>
     <h1>UAMovie Login</h1>
     <form action="" method="POST">
-      Username: <input type = "text" name = "username"><br>
-      Password: <input type = "password" name = "password"><br>
+      Username: <input type = "text" name = "username"><br><br>
+      Password: <input type = "password" name = "password"><br><br>
       <input type = "submit" value = " Submit "/>
     </form>
   </body>
