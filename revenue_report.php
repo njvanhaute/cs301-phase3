9f9f157889609332d8f28f7595e622fa7832beb3
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2018-12-02 23:24:15 +0000

    Finished revenue_report.php

--- revenue_report.php
+++ revenue_report.php
@@ -11,15 +11,62 @@
   }
 
 
-  $adult_cost = 11.54;
-  $senior_cost = 9.23;
-  $child_cost = 8.08;
-
-  $months_sql = "SELECT ORDER_DATE FROM ORDER_ITEM;"; 
-  $result = mysqli_query($db, $months_sql);
-  $months = [];
+  $revenue_sql = "SELECT LEFT(ORDER_DATE, 2) AS MONTH_NO, 9.23 * SUM(Num_Senior_Tickets) AS SENIOR_REV, 8.08 * SUM(Num_Child_Tickets) AS CHILD_REV, 11.54 * SUM(Num_Adult_Tickets) AS ADULT_REV FROM ORDER_ITEM WHERE ORDER_STATUS = \"completed\" GROUP BY MONTH_NO;"; 
+  $result = mysqli_query($db, $revenue_sql);
   $row = mysqli_fetch_assoc($result);
-  $result["ORDER_DATE"] // returns date value for that row; 
+  $rev_info = [];
+  while (!is_null($row)) {
+    array_push($rev_info, $row); // returns date value for that row; 
+    $row = mysqli_fetch_assoc($result);
+  }
+
+  $totals = [];
+  for ($i = 0; $i < 3; $i++) {
+    $totals[$i] = $rev_info[$i]["SENIOR_REV"] + $rev_info[$i]["CHILD_REV"] + $rev_info[$i]["ADULT_REV"];
+  }
+
+  function month_from_no($no) {
+    switch ($no) {
+      case "01":
+        return "January";
+        break;
+      case "02":
+        return "February";
+        break;
+      case "03":
+        return "March";
+        break;
+      case "04":
+        return "April";
+        break;
+      case "05":
+        return "May";
+        break;
+      case "06":
+        return "June";
+        break;
+      case "07":
+        return "July";
+        break;
+      case "08":
+        return "August";
+        break;
+      case "09":
+        return "September";
+        break;
+      case "10":
+        return "October";
+        break;
+      case "11":
+        return "November";
+        break;
+      case "12":
+        return "December";
+        break;
+      default:
+        return "Error";
+    }
+  }
 ?>
  
 <!DOCTYPE html>
@@ -34,20 +81,16 @@
 </head>
 <body>
     <div class="page-header">
-        <h1><b><?php echo $movie_title ?></b></h1>
+        <h1><b><?php echo "View Revenue Report" ?></b></h1>
     </div>
-    <table style='margin: auto; width: 75%;' class='table-bordered'>
-      <tr><th style='font-size: 20px; padding: 10px;'>Release Date</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_date; ?></td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Rating</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_rating; ?></td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Movie Length</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_length; ?> minutes</td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Movie Genre</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_genre; ?></td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Movie Average Rating</th><td style='font-size: 20px; padding: 10px;'><?php echo bcdiv($movie_avg, 1, 1); ?>/5 stars</td></tr>
+    <table style='margin: auto; width: 50%;' class='table-bordered'>
+      <tr><th style='font-size: 20px; padding: 10px;'><?php echo month_from_no($rev_info[0]["MONTH_NO"]); ?></th><td style='font-size: 20px; padding: 10px;'><?php echo $totals[0]; ?></td></tr>
+      <tr><th style='font-size: 20px; padding: 10px;'><?php echo month_from_no($rev_info[1]["MONTH_NO"]); ?></th><td style='font-size: 20px; padding: 10px;'><?php echo $totals[1]; ?></td></tr>
+      <tr><th style='font-size: 20px; padding: 10px;'><?php echo month_from_no($rev_info[2]["MONTH_NO"]); ?></th><td style='font-size: 20px; padding: 10px;'><?php echo $totals[2]; ?></td></tr>
     </table><br>
     <br>
     <p>
-      <a class="btn btn-primary" href="overview.php" style="width: 150px; font-size: 20px;">Overview</a>
-      <a class="btn btn-primary" href="review.php" style="width: 150px; font-size: 20px;">Movie Review</a>
-      <a class="btn btn-primary" href="ticket.php" style="width: 150px; font-size: 20px;">Buy Ticket</a>
+      <a class="btn btn-danger" href="manager.php" style="width: 150px; font-size: 20px;">Back</a>
     </p>
 </body>
 </html>
