a713ff5232cb6e08cd58bca033ffe79ac9fa31e4
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2018-12-03 02:08:35 +0000

    Cleaning up file

--- src/nowplaying.php
+++ src/nowplaying.php
@@ -6,53 +6,7 @@
   if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       header("location: login.php");
       exit;
-  }
-
-  if ($movie_title == 'Beautiful Boy'){
-    $img = 'images/beautiful_boy.jpg';
-  }
-  if ($movie_title == 'Bohemian Rhapsody'){
-    $img = 'images/bohemian_rhapsody.jpg';
-  }
-  if ($movie_title == 'Boy Erased'){
-    $img = 'images/boy_erased.jpg';
-  }
-  if ($movie_title == 'Creed II'){
-    $img = 'images/creed_ii.jpg';
-  }
-  if ($movie_title == 'Fantastic Beasts: The Crimes of Grindelwald'){
-    $img = 'images/fantastic_beasts.jpg';
-  }
-  if ($movie_title == 'The Front Runner'){
-    $img = 'images/front_runner.jpg';
-  }
-  if ($movie_title == "Dr. Seuss’ The Grinch"){
-    $img = 'images/grinch.jpg';
-  }
-  if ($movie_title == 'Instant Family'){
-    $img = 'images/instant_family.jpg';
-  }
-  if ($movie_title == 'The Nutcracker and the Four Realms'){
-    $img = 'images/nutcracker.jpg';
-  }
-  if ($movie_title == 'Ralph Breaks the Internet'){
-    $img = 'images/ralph.jpg';
-  }
-  if ($movie_title == 'Robin Hood'){
-    $img = 'images/robin_hood.jpg';
-  }
-  if ($movie_title == 'Smallfoot'){
-    $img = 'images/smallfoot.jpg';
-  }
-  if ($movie_title == 'A Star is Born'){
-    $img = 'images/star_is_born.jpg';
-  }
-  if ($movie_title == 'Venom'){
-    $img = 'images/venom.jpg';
-  }
-  if ($movie_title == 'Widows'){
-    $img = 'images/widows.jpg';
-  }
+  } 
 ?>
 
 <!DOCTYPE html>
