76254ce2b87f1d9233a9fa432dfc8ad60bdf4ea8
aam659 <manningalan659@gmail.com>
2018-12-02 19:26:12 -0600

    Updated HTML and popular movies php file

--- pop_movie_report.php
+++ pop_movie_report.php
@@ -17,11 +17,54 @@
 
   $top_sellers_sql = "select Title, MONTH_NO, SUMTOT from ( select Title, MONTH_NO, SUMTOT, (@rn:=if(@prev = MONTH_NO, @rn +1, 1)) as rownumb, @prev:= MONTH_NO from ( select Title, MONTH_NO, SUM(Num_Total_Tickets) AS SUMTOT from ORDER_ITEM_W_MONTH where Order_Status <> \"cancelled\" GROUP BY Title, MONTH_NO order by MONTH_NO, SUMTOT desc, Title ) as sortedlist JOIN (select @prev:=NULL, @rn :=0) as vars ) as groupedlist where rownumb<=3 order by MONTH_NO, SUMTOT desc, Title;"; 
   $result = mysqli_query($db, $top_sellers_sql);
-  $months = [];
+  $month_info = [];
   $row = mysqli_fetch_assoc($result);
   $row["ORDER_DATE"]; // Returns date value for that row;
   $row["TITLE"]; // Returns most popular movie titles
   $row["NUM_TOTAL_TICKETS"]; // Returns total number of tickets ordered
+
+  function month_from_no($no) {
+    switch ($no) {
+      case "01":
+        return "January";
+        break;
+      case "02":
+        return "February";
+        break;
+      case "03":
+        return "March";
+        break;
+      case "04":
+        return "April";
+        break;
+      case "05":
+        return "May";
+        break;
+      case "06":
+        return "June";
+        break;
+      case "07":
+        return "July";
+        break;
+      case "08":
+        return "August";
+        break;
+      case "09":
+        return "September";
+        break;
+      case "10":
+        return "October";
+        break;
+      case "11":
+        return "November";
+        break;
+      case "12":
+        return "December";
+        break;
+      default:
+        return "Error";
+    }
+  }
 ?>
  
 <!DOCTYPE html>
@@ -36,14 +79,17 @@
 </head>
 <body>
     <div class="page-header">
-        <h1><b><?php echo $movie_title ?></b></h1>
+        <h1><b><?php echo "View Popular Movie Report" ?></b></h1>
     </div>
-    <table style='margin: auto; width: 75%;' class='table-bordered'>
-      <tr><th style='font-size: 20px; padding: 10px;'>Release Date</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_date; ?></td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Rating</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_rating; ?></td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Movie Length</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_length; ?> minutes</td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Movie Genre</th><td style='font-size: 20px; padding: 10px;'><?php echo $movie_genre; ?></td></tr>
-      <tr><th style='font-size: 20px; padding: 10px;'>Movie Average Rating</th><td style='font-size: 20px; padding: 10px;'><?php echo bcdiv($movie_avg, 1, 1); ?>/5 stars</td></tr>
+    <table style='margin: auto; width: 50%;' class='table-bordered'>
+      <tr>
+        <th style='font-size: 20px; padding: 10px;'><?php echo month_from_no($rev_info[0]["MONTH_NO"]); ?></th>
+        <th style='font-size: 20px; padding: 10px;'><?php echo "Movie"; ?></th>
+        <th style='font-size: 20px; padding: 10px;'><?php echo "#of Orders"; ?></th>
+      </tr>
+      <tr>
+        
+      </tr>
     </table><br>
     <br>
     <p>
